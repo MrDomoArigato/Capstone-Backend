8c72fe8b0ab4feb09c8d4eb58cce3f6c876e8806
Andres <90808055+AndresMedin@users.noreply.github.com>
2024-02-21 11:06:01 -0600

    Add files via upload

---
+++ Models/Accounts.cs
@@ -0,0 +1,9 @@
+//Account Model
+public class Account
+{
+    public int AccountId { get; set; } 
+    public required string AccountName { get; set; } 
+    public required string AccountOwner { get; set; } 
+    public decimal Balance { get; set; }
+    public required ICollection<Transaction> Transactions { get; set; } 
+}
