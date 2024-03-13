3a75dce90ad928bb76ac2b64295b972f96a8c344
MrDomoArigato <trash@akikato42.dev>
2024-03-13 12:02:14 -0500

    Revert "AndresMedin-patch-1"

--- Program.cs
+++ Program.cs
@@ -1,29 +1,13 @@
-using CapstoneApi.Database;
-using CapstoneApi.Services;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.ClearProviders().AddConsole();
-
-//Gets DB connect from secrets 
-var dbconnection = builder.Configuration.GetSection("db-connection").Get<DBConnection>();
-
 // Add services to the container.
+
 builder.Services.AddControllers();
-//Sets up database connection for reference
-builder.Services.AddDbContext<CapstoneContext>(options => {
-                options.UseNpgsql($"Host={dbconnection!.Host}:{dbconnection!.Port};Database={dbconnection!.Database};Username={dbconnection!.Username};Password={dbconnection!.Password}");
-            });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddScoped<IAccountService, AccountService>();
-builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-app.MapGet("/", () => dbconnection);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,9 +16,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
 app.MapControllers();
 
--- README.md
+++ README.md
@@ -3,10 +3,3 @@ Backendend Portion of the Capstone Project at UMKC
 
 ## Requirements
 - Project uses .NET SDK 8.0
-
-## Database Setup
-- `dotnet user-secrets set "db-connection:Host" "<Host>"`
-- `dotnet user-secrets set "db-connection:Port" "<Port>"`
-- `dotnet user-secrets set "db-connection:Username" "<Username>"`
-- `dotnet user-secrets set "db-connection:Password" "<Password>"`
-- `dotnet user-secrets set "db-connection:Database" "<Database>"`
